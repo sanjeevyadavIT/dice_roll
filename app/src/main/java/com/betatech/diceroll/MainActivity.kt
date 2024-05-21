85532521cad0faf0b817b0f1e9fb6bf74a1b95a2
Sanjeev Yadav <sanjeevy133@protonmail.com>
2024-05-21 15:59:22 +0530

    Add multiple dice based on selection
    f

--- app/src/main/java/com/betatech/diceroll/MainActivity.kt
+++ app/src/main/java/com/betatech/diceroll/MainActivity.kt
@@ -5,12 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,10 +30,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.betatech.diceroll.ui.theme.DiceRollTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,7 +47,9 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     App(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
                     )
                 }
             }
@@ -43,10 +59,49 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App(name: String, modifier: Modifier = Modifier) {
-    var result by remember {
-        mutableStateOf(1)
+    var numberOfDice by remember {
+        mutableStateOf(1.0f)
     }
-    val imageResource = when (result) {
+    var diceNumbers by remember {
+        mutableStateOf(IntArray(numberOfDice.toInt()){ 1 })
+    }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.padding(16.dp)
+        ) {
+            items(diceNumbers.size) { index ->
+                Dice(diceNumbers[index])
+            }
+        }
+        Text(text = "Number of dice")
+        Slider(
+            value = numberOfDice,
+            onValueChange = {
+                diceNumbers = IntArray(it.toInt()) { (1..6).random() }
+                numberOfDice = it.roundToInt().toFloat()
+            },
+            valueRange = 1f..10f,
+            steps = 9,
+
+        )
+        Button(onClick = { diceNumbers = IntArray(numberOfDice.toInt()) { (1..6).random() } }) {
+            Text(text = stringResource(R.string.spin))
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Total: ${numberOfDice}")
+    }
+}
+
+
+@Composable
+fun Dice(diceNumber: Int, modifier: Modifier = Modifier) {
+    val imageResource = when (diceNumber) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
         3 -> R.drawable.dice_3
@@ -54,16 +109,7 @@ fun App(name: String, modifier: Modifier = Modifier) {
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.fillMaxSize()
-    ) {
-        Image(painter = painterResource(imageResource), contentDescription = null)
-        Button(onClick = { result = (1..6).random() }) {
-            Text(text = stringResource(R.string.spin))
-        }
-    }
+    Image(painter = painterResource(imageResource), contentDescription = null)
 }
 
 @Preview(showBackground = true)
