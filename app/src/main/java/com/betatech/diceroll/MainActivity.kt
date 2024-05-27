85adc4c3ed540f351364281bd770671b9051357f
Sanjeev Yadav <sanjeevy133@protonmail.com>
2024-05-27 23:22:19 +0530

    Add font and AppBar

--- app/src/main/java/com/betatech/diceroll/MainActivity.kt
+++ app/src/main/java/com/betatech/diceroll/MainActivity.kt
@@ -10,6 +10,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,7 +33,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DiceRollTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        DiceRollAppBar()
+                    }
+                ) { innerPadding ->
                     App(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -39,6 +47,14 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DiceRollAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(title = {
+        Text(text = stringResource(id = R.string.app_name), style = MaterialTheme.typography.headlineMedium)
+    })
 }
 
 @Composable
